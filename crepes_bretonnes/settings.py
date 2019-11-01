c24bcfd71e376c7aad0f2388e5197f6e5c808cb7
Jaime Sastre <jaime.sastre@lapiscine.pro>
2019-11-02 00:09:36 +0100

    sauvegarde 2019-11-retour

--- crepes_bretonnes/settings.py
+++ crepes_bretonnes/settings.py
@@ -11,7 +11,7 @@ https://docs.djangoproject.com/en/2.2/ref/settings/
 """
 
 import os
-import django_heroku
+#import django_heroku
 from django.core.mail.backends.smtp import EmailBackend
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
