5f60e3997f47ad0865dc231c8034d952a9112138
Jaime Sastre <jaime.sastre@lapiscine.pro>
2019-11-01 10:14:44 +0100

    Postgre

--- crepes_bretonnes/settings.py
+++ crepes_bretonnes/settings.py
@@ -115,17 +115,27 @@ WSGI_APPLICATION = 'crepes_bretonnes.wsgi.application'
 # }
 
 
+#DATABASES = {
+#   'default': {
+#        'ENGINE': 'django.db.backends.mysql',
+#        'PORT': '8889',
+#        'HOST':'127.0.0.1',     # 'localhost','127.0.0.1'
+#        'NAME': 'crepes_bretonnes',
+#        'USER': 'root',
+#        'PASSWORD': 'root',
+#   }
+#}
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.mysql',
-        'PORT': '8889',
-        'HOST':'127.0.0.1',     # 'localhost','127.0.0.1'
-        'NAME': 'crepes_bretonnes',
-        'USER': 'root',
-        'PASSWORD': 'root',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'mydatabase',
+        'USER': 'mydatabaseuser',
+        'PASSWORD': 'mypassword',
+        'HOST': '127.0.0.1',
+        'PORT': '5432',
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/2.2/ref/settings/#auth-password-validators
@@ -187,3 +197,5 @@ TEMPLATE_DIRS = (
 )
 
 APPEND_SLASH = True # ajout de slash en fin d'URL
+
+django_heroku.settings(locals())
