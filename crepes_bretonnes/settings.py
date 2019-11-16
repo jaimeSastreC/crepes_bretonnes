a4a9fcf700385d26ac33b18a4f506bb7df059615
Jaime Sastre <jaime.sastre@lapiscine.pro>
2019-11-17 00:37:13 +0100

    sauvegarde 2019-11-retour 2

--- crepes_bretonnes/settings.py
+++ crepes_bretonnes/settings.py
@@ -14,9 +14,9 @@ import os
 #import django_heroku
 from django.core.mail.backends.smtp import EmailBackend
 
-# Todo : vérifier impact mise à jour chemin root
+
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
-BASE_DIR = os.path.dirname(os.path.abspath(__file__))
+BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
 
 # Quick-start development settings - unsuitable for production
